ca8e511d4949eb6721e1cc89fb6ac51f05272a2b
Stan Sajous <stan.sajous@boweryvaluation.com>
2021-10-12 11:31:26 -0600

    basic skeleton in place

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/base.css'
+import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <section class="todoapp">
+      <App />
+    </section>
   </React.StrictMode>,
   document.getElementById('root')
 );
